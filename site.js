2e4654983ce29121d9bfbc05303ec8132850826b
WANMIN ZHANG <wzhang90@hawk.iit.edu>
2019-09-14 20:31:20 -0500

    fix

--- site.js
+++ site.js
@@ -15,18 +15,17 @@
   // Display data from object self in the header
   document.getElementById("myHeader").innerHTML = self.fullOutput();
 
-  // Make function for my courses
-  function myCourses() {
-    // For loop to go through the arrays
-    for (var i = 0; i < courseNum.length; i++) {
-      document.write(courseCat + courseNum + courseName);
-      i++;
-    }
-  }
-
   // Create an array for course categories
   let courseCat = ["COM", "COM", "COM", "ITMD", "ITMD"];
   // Create an array for course numbers
   let courseNum = ["372", "380", "428", "462", "463"];
   // Create an array for course titles
   let courseName = ["Mass Media Society", "Social Media & Society", "Verbal Visual Communications", "Website App Development", "Intermediate Web App Development"];
+
+  // Make function for my courses
+  function myCourses() {
+    // For loop to go through the arrays
+    for (var i = 0; i < courseNum.length; i++) {
+      document.write(courseCat + courseNum + courseName);
+    }
+  }
