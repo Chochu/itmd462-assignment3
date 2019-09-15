878a1709e13240f3fca77a2fe6e8b15361481c8e
WANMIN ZHANG <wzhang90@hawk.iit.edu>
2019-09-14 20:28:55 -0500

    Assignment3

---
+++ site.js
@@ -0,0 +1,32 @@
+// Create an object (myself)
+  var self = {
+    fName: "Wanmin",
+    lName: "Zhang",
+    season: "Fall",
+    year: "2019",
+    fullOutput: function (){
+      // Make use of the "this" keyword
+      return this.fName + " " + this.lName + " " + this.season + " " + this.year;
+    }
+  };
+
+  console.log('object done');
+
+  // Display data from object self in the header
+  document.getElementById("myHeader").innerHTML = self.fullOutput();
+
+  // Make function for my courses
+  function myCourses() {
+    // For loop to go through the arrays
+    for (var i = 0; i < courseNum.length; i++) {
+      document.write(courseCat + courseNum + courseName);
+      i++;
+    }
+  }
+
+  // Create an array for course categories
+  let courseCat = ["COM", "COM", "COM", "ITMD", "ITMD"];
+  // Create an array for course numbers
+  let courseNum = ["372", "380", "428", "462", "463"];
+  // Create an array for course titles
+  let courseName = ["Mass Media Society", "Social Media & Society", "Verbal Visual Communications", "Website App Development", "Intermediate Web App Development"];
