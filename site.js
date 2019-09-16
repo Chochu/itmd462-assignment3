832e818ddd7aee3c122c1b34ff5b691b6241cfd5
WANMIN ZHANG <wzhang90@hawk.iit.edu>
2019-09-16 15:17:32 -0500

    Upload Assignment 3 code files

--- site.js
+++ site.js
@@ -13,17 +13,17 @@ var self = {
 // Display data from object self in the header
 document.getElementById("myHeader").innerHTML = self.fullOutput();
 
-// Create an array for course categories
-let courseCat = ["COM", "COM", "COM", "ITMD", "ITMD"];
-// Create an array for course numbers
-let courseNum = ["372", "380", "428", "462", "463"];
-// Create an array for course titles
-let courseName = ["Mass Media Society", "Social Media & Society", "Verbal Visual Communications", "Website App Development", "Intermediate Web App Development"];
-
 // Make function for my courses
 function myCourses() {
+  // Create an array for course categories
+  let courseCat = ["COM", "COM", "COM", "ITMD", "ITMD"];
+  // Create an array for course numbers
+  let courseNum = ["372", "380", "428", "462", "463"];
+  // Create an array for course titles
+  let courseName = ["Mass Media Society", "Social Media & Society", "Verbal Visual Communications", "Website App Development", "Intermediate Web App Development"];
+
   // For loop to go through the arrays
-  for (var i = 0; i < courseNum.length; i++) {
-    document.write(courseCat + courseNum + courseName);
+  for (var i = 0; i < courseCat.length; i++) {
+    document.write(courseCat[i].bold() + courseNum[i].bold() + "-" + courseName[i];
   }
 }
